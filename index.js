dade4767ed5b9cf2e3fb4d0b55608fc40e6136dd
felipescht709 <156364363+felipescht709@users.noreply.github.com>
2025-04-01 21:10:28 -0300

    Add files via upload
    Implementada a conexão com o banco de dados.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import express from "express";
-
+import {Sequelize} from "sequelize";
+const sequelize = new Sequelize('biblioteca2025', 'postgres', '#Felipe123', {
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    define: {
+        timestamps: false,
+        freezeTableName: true
+    }
+  });
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com o banco de dados realizada com sucesso.');
+  } catch (error) {
+    console.error('Erro ao conectar com o banco de dados:', error);
+  }
 const app = express();
 app.use(express.json());
 app.get('/teste', (req, res)=>{
